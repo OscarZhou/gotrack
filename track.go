8e1f7c6f5bb3bad440ba78ad9b377bc56155821c
OscarZhou <zhouhongyu1989@gmail.com>
2019-02-10 21:04:28 +1300

    Add inProgress feature. When Asyn is true, the log will be printed every second automatically

--- track.go
+++ track.go
@@ -13,6 +13,8 @@ type CallerName string
 const (
 	// PhaseStart stands for function start phase
 	PhaseStart = "Start"
+	// PhaseInProgress stands for function is running
+	PhaseInProgress = "InProgress"
 	// PhaseEnd stands for function end phase
 	PhaseEnd = "End"
 )
@@ -20,16 +22,20 @@ const (
 // Track records all track information
 type Track struct {
 	Debug   bool
+	AsynLog bool
 	mutex   *sync.Mutex
 	callers map[CallerName]time.Time
+	tickers map[CallerName]*time.Ticker
 }
 
 //
 func Default() *Track {
 	return &Track{
 		Debug:   true,
+		AsynLog: true,
 		mutex:   &sync.Mutex{},
 		callers: make(map[CallerName]time.Time),
+		tickers: make(map[CallerName]*time.Ticker),
 	}
 }
 
@@ -38,7 +44,20 @@ func (t *Track) Start() {
 	if t.Debug {
 		curCaller := t.callerName()
 		t.callers[curCaller] = time.Now()
+		if t.AsynLog {
+			go t.inProgress(curCaller)
+		}
 		t.print(PhaseStart, curCaller, 0)
+
+	}
+}
+
+func (t *Track) inProgress(s CallerName) {
+	if t.Debug {
+		t.tickers[s] = time.NewTicker(time.Second)
+		for range t.tickers[s].C {
+			t.print(PhaseInProgress, s, 0)
+		}
 	}
 }
 
@@ -46,6 +65,10 @@ func (t *Track) Start() {
 func (t *Track) End() {
 	if t.Debug {
 		curCaller := t.callerName()
+		if t.AsynLog {
+			t.tickers[curCaller].Stop()
+			delete(t.tickers, curCaller)
+		}
 		elapsed := time.Since(t.callers[curCaller])
 		t.print(PhaseEnd, curCaller, elapsed)
 		delete(t.callers, curCaller)
@@ -59,6 +82,8 @@ func (t *Track) print(p string, s CallerName, elapsed time.Duration) {
 
 	switch p {
 	case PhaseStart:
+		fmt.Printf("%s function:\t%v \n", p, string(s))
+	case PhaseInProgress:
 		fmt.Printf("%s function:\t%v \n", p, string(s))
 	case PhaseEnd:
 		fmt.Printf("%s function:\t%v took %v \n", p, string(s), elapsed)
